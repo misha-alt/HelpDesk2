915a1b17f0f0a086e376850caf3340fec6502adb
Your NameMisha <mikhaily0@mail.ru>
2022-11-05 12:39:02 +0300

    TestLgic

---
+++ src/main/java/misha/service/CreateComment.java
@@ -0,0 +1,100 @@
+package misha.service;
+
+
+import misha.domain.Comments;
+import misha.domain.User;
+import org.dom4j.Comment;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
+
+@Repository
+public class CreateComment {
+
+    private SessionFactory sessionFactory;
+    private UserService userService;
+    @Autowired
+    public CreateComment(SessionFactory sessionFactory, UserService userService) {
+        this.sessionFactory = sessionFactory;
+        this.userService = userService;
+    }
+
+    public String madeComment (){
+        LocalDate date = LocalDate.now();
+        return date.toString();
+    }
+
+    public void updateUsersComment (User user){
+        Session session = sessionFactory.getCurrentSession();
+        session.update(user);
+    }
+
+        public void seveUserCmments (Comments comments){
+            createComentAndSave(comments);
+
+            User user = userService.getOurUser();
+            user.getComments().add( getById(comments.getId()));
+
+      // userService.getOurUser().getComments().add( getById(comments.getId()));
+
+
+        updateUsersComment(user);
+
+
+
+
+        }
+
+        public void createComentAndSave(Comments comments){
+            Session session = sessionFactory.getCurrentSession();
+            session.save(comments);
+        }
+
+
+
+    public Comments getById(int id) {
+        Query query = sessionFactory.getCurrentSession().createQuery("from Comments p where p.id = :id");
+        query.setParameter("id", id);
+        return (Comments) query.list().get(0);
+    }
+
+    public Comments getByUser_Id(int id) {
+        Query query = sessionFactory.getCurrentSession().createQuery("from Comments p where p.user_id = :id");
+        query.setParameter("id", id);
+        return (Comments) query.list();
+    }
+
+    public List<Comments> getAll() {
+        Query query = sessionFactory.getCurrentSession().createQuery("from Comments");
+        return query.list();
+    }
+
+
+
+
+    public void testsMet(){
+
+        Comments comments = new Comments();
+        comments.setId(1);
+        comments.setComment("first Comment");
+        comments.setDate("2022-12-12");
+        Comments comments2 = new Comments();
+        comments2.setId(2);
+        comments2.setComment("second Comment");
+        comments2.setDate("2022-12-13");
+        User user = userService.getOurUser();
+        user.getComments().add(comments);
+        user.getComments().add(comments2);
+    }
+
+
+
+
+
+}
